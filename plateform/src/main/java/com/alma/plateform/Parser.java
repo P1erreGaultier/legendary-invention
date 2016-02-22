06972bd6ae358445729fb0a175a6e0513f65c8fe
nadhirMessaoud <messaoud.nadhir@gmail.com>
2016-02-22 17:13:54 +0100

    finish parser

--- plateform/src/main/java/com/alma/plateform/Parser.java
+++ plateform/src/main/java/com/alma/plateform/Parser.java
@@ -5,50 +5,38 @@ import com.alma.plateform.Plugin;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
 public class Parser {
 
-	String fileName;
 	String aLine;
 
-    public static void main(String[] args) {
+	public List<Plugin> parseIt(String formatFichier) throws IOException{
 
-        Parser myParser = new Parser();
-        Plugin p= new Plugin();
-        try {
-            p = myParser.parseIt("formatFichier.txt");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(p.getProperties().getProperty("class"));
-
-    }
-
-
-	public Plugin parseIt(String formatFichier) throws IOException{
-
-		FileReader fileReader = new FileReader(fileName);
+		FileReader fileReader = new FileReader(formatFichier);
 		BufferedReader reader = new BufferedReader(fileReader);
-		Plugin plug = new Plugin();
-		//aLine = reader.readLine();
+
+        List<Plugin> res = new ArrayList<>();
 		int i = 0;
         Properties properties = new Properties();
         while (reader.ready()) {
 
 			aLine = reader.readLine();
-
+            Plugin plug = new Plugin();
 			String[] OneAttributeOneValue = aLine.split(";");
             for (int c =0; c< OneAttributeOneValue.length; c++){
-                properties.setProperty(OneAttributeOneValue[c].split("=")[1],OneAttributeOneValue[c].split("=")[2]);
+
+                properties.setProperty(OneAttributeOneValue[c].split("=")[0],OneAttributeOneValue[c].split("=")[1]);
             }
 
 			plug.setProperties(properties);
+            res.add(plug);
 		}
 		reader.close();
-		return plug;
+		return res;
 	}
 
 
