bb3ad5d5682117a05ab2a4997ed1c3efadb410ed
Folkvir <folkvir@outlook.fr>
2016-03-07 17:11:57 +0100

    add sample extension

---
+++ extensions/sample-extension/src/main/java/com/extensions/sample/Sample.java
@@ -0,0 +1,13 @@
+
+package com.extensions.sample;
+
+import com.alma.application.interfaces.IMonster;
+
+public class Sample implements IMonster {
+
+    	public Sample() {
+        	// ...
+    	}
+		
+}
+
