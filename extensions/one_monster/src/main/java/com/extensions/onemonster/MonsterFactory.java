92464968dcdc3739a4a0749d054ec182b7a73930
P1erreGaultier <pit.gaultier@free.fr>
2016-03-07 17:53:09 +0100

    change on random monster + images

--- extensions/one_monster/src/main/java/com/extensions/onemonster/MonsterFactory.java
+++ extensions/one_monster/src/main/java/com/extensions/onemonster/MonsterFactory.java
@@ -12,27 +12,26 @@ public class MonsterFactory implements IMonsterFactory {
     public IMonster createMonster20(){
         Random randomgenerator = new Random();
         IMonster m = new Monster();
-        m.setHp(1 + randomgenerator.nextInt(20));
-        m.setImage(this.getClass().getResource("monster.png").getPath());
-        /*switch (randomgenerator.nextInt(4)){
+
+        switch (randomgenerator.nextInt(4)){
             case 0:
-                m.setImage(this.getClass().getResource("monster.png").getPath());
+                m.setImage(this.getClass().getResource("bob.png").getPath());
                 break;
             case 1:
 
-                m.setImage(this.getClass().getResource("monster.png").getPath());
+                m.setImage(this.getClass().getResource("gremlins-gizmo.jpg").getPath());
                 break;
             case 2:
 
-                m.setImage(this.getClass().getResource("monster.png").getPath());
+                m.setImage(this.getClass().getResource("pascal.jpg").getPath());
                 break;
             case 3:
 
-                m.setImage(this.getClass().getResource("monster.png").getPath());
+                m.setImage(this.getClass().getResource("terriblvert.jpg").getPath());
                 break;
             default:
                 break;
-        }*/
+        }
         return m;
     }
 }
