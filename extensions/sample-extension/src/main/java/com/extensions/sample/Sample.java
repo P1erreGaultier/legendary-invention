a28881ae18a8252e335599ec8b3fae091c1f0daf
Folkvir <folkvir@outlook.fr>
2016-03-07 17:17:21 +0100

    sample extension done

--- extensions/sample-extension/src/main/java/com/extensions/sample/Sample.java
+++ extensions/sample-extension/src/main/java/com/extensions/sample/Sample.java
@@ -8,6 +8,25 @@ public class Sample implements IMonster {
     	public Sample() {
         	// ...
     	}
-		
+
+    @Override
+    public int getHp() {
+        return 0;
+    }
+
+    @Override
+    public void setHp(int i) {
+
+    }
+
+    @Override
+    public String getImage() {
+        return null;
+    }
+
+    @Override
+    public void setImage(String s) {
+
+    }
 }
 
