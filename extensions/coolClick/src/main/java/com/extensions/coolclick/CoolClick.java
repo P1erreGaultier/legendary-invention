1c004fa58f1686eaa2f867c38b57e7c6747ebafa
Folkvir <folkvir@outlook.fr>
2016-03-07 17:56:31 +0100

    add extensions/coolclick && New click cursor

---
+++ extensions/coolClick/src/main/java/com/extensions/coolclick/CoolClick.java
@@ -0,0 +1,27 @@
+
+package com.extensions.coolclick;
+
+import com.alma.application.interfaces.handler.IClickHandler;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class CoolClick implements IClickHandler {
+
+    	public CoolClick() {
+
+    	}
+
+        public void setHandler(JLabel label){
+            label.setCursor(
+                    Toolkit.getDefaultToolkit().createCustomCursor(
+                            new ImageIcon("cursor.png").getImage(),
+                            new Point(0,0),
+                            "Target cursor")
+            );
+        }
+
+}
+
